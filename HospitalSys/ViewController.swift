70c98f40a7d3475df68c719064d3cf054e65d5be
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2016-11-23 14:38:09 +0100

    Initial Commit

---
+++ HospitalSys/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HospitalSys
+//
+//  Created by krzysiek on 23.11.2016.
+//  Copyright © 2016 krzysiek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
