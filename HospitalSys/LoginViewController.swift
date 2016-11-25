4d15970549b55d6af3559fa0aa452feb673bd481
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2016-11-25 16:30:55 +0100

    added visits table view and progress indicator when logging

--- HospitalSys/LoginViewController.swift
+++ HospitalSys/LoginViewController.swift
@@ -12,8 +12,11 @@ class LoginViewController: NSViewController {
     
     weak var containerVC: ContainerViewController?
     
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
     
     @IBOutlet weak var usernameTextField: NSTextField!
+    
+    @IBOutlet weak var loginButton: NSButton!
     
     @IBOutlet weak var passwordTextField: NSSecureTextField!
     
@@ -25,17 +28,33 @@ class LoginViewController: NSViewController {
         if textFieldsAreValid() {
             print("zalogowano")
             WebserviceConnector.login(loginVC: self)
+            startShowingProgress()
         }
         
     }
     
+    fileprivate func startShowingProgress() {
+        progressIndicator.isHidden = false
+        progressIndicator.startAnimation(nil)
+        loginButton.isEnabled = false
+    }
+    
+    
+    fileprivate func stopShowingProgress() {
+        progressIndicator.stopAnimation(nil)
+        progressIndicator.isHidden = true
+        loginButton.isEnabled = true
+    }
+    
     func loginSuccessful() {
         print("login successful")
+        stopShowingProgress()
         containerVC?.loginSuccessful()
     }
     
     func loginUnsuccessful() {
         print("login unsuccessful")
+        stopShowingProgress()
         infoLabel.stringValue = "Nie udało się zalogować"
     }
     
@@ -47,6 +66,7 @@ class LoginViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         infoLabel.stringValue = ""
+        progressIndicator.isHidden = true
     }
     
     fileprivate func textFieldsAreValid() -> Bool{
--- HospitalSys/VisitsViewController.swift
+++ HospitalSys/VisitsViewController.swift
@@ -8,7 +8,23 @@
 
 import Cocoa
 
+class ScheduledVisit {
+    let date: String
+    let doctorsName: String
+    
+    init(_ date: String, doctorsName: String) {
+        self.date = date
+        self.doctorsName = doctorsName
+    }
+}
+
 class VisitsViewController: NSViewController {
+    
+    let schedule = [ScheduledVisit("27.09.2016", doctorsName: "dr Lubicz"),
+                    ScheduledVisit("28.09.2016", doctorsName: "dr Lubicz"),
+                    ScheduledVisit("29.09.2016", doctorsName: "dr Lubicz"),
+                    ScheduledVisit("30.09.2016", doctorsName: "dr Lubicz")
+                    ]
     
     @IBOutlet weak var visitsTableView: NSTableView! {
         didSet {
@@ -19,15 +35,42 @@ class VisitsViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for date in schedule {
+            print(date.date, date.doctorsName)
+        }
         // Do view setup here.
     }
     
 }
 
 extension VisitsViewController: NSTableViewDelegate {
-    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        var text = ""
+        var cellIdentifier = ""
+        
+        let item = schedule[row]
+        
+        if tableColumn == tableView.tableColumns[0] {
+            text = item.date
+            cellIdentifier = "DateCellID"
+        }
+        else if tableColumn == tableView.tableColumns[1] {
+            text = item.doctorsName
+            cellIdentifier = "NameCellID"
+        }
+        
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+        return nil
+    }
 }
 
 extension VisitsViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return schedule.count
+    }
     
 }
