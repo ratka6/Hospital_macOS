3e4358ed5bb8205fd57a977e74ef4b6536b296b1
Krzysztof <krzysztof.fryzlewicz@spinney.io>
2016-11-25 18:57:05 +0100

    added new datepicker

--- HospitalSys/NewAppointmentViewController.swift
+++ HospitalSys/NewAppointmentViewController.swift
@@ -12,15 +12,41 @@ class NewAppointmentViewController: NSViewController {
     
     @IBOutlet weak var specialtyComboBox: NSComboBox!
     @IBOutlet weak var doctorsNameComboBox: NSComboBox!
-    @IBOutlet weak var datePicker: NSDatePicker!
+    @IBOutlet weak var datePicker: NMDatePicker!
     
     @IBAction func makeAppointmentButtonClicked(_ sender: Any) {
         dismiss(self)
     }
     
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+        dismiss(self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDatePicker()
         // Do view setup here.
     }
     
+    fileprivate func setupDatePicker() {
+        datePicker.dateValue = Date()
+        datePicker.delegate = self
+        datePicker.backgroundColor = NSColor.white
+        datePicker.titleFont = NSFont.boldSystemFont(ofSize: 14.0)
+        datePicker.textColor = NSColor.black
+        datePicker.todayTextColor = NSColor.blue
+        datePicker.selectedTextColor = NSColor.white
+        datePicker.todayBackgroundColor = NSColor.blue
+        datePicker.todayBorderColor = NSColor.blue
+        datePicker.highlightedBackgroundColor = NSColor.lightGray
+        datePicker.highlightedBorderColor = NSColor.darkGray
+        datePicker.selectedBackgroundColor = NSColor.orange
+        datePicker.selectedBorderColor = NSColor.blue
+    }
+    
+}
+
+extension NewAppointmentViewController: NMDatePickerDelegate {
+    func nmDatePicker(_ datePicker: NMDatePicker, selectedDate: Date) {
+        
+    }
 }
